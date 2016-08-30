dc5e2e29dfc79879a67449b8ef1cb9156914f9dc
mrnelley <kelleysoftware@gmail.com>
2016-08-30 12:29:08 -0400

    hello world

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 //*****************NPM DEPENDENCY INJECTION******************
-var express    = require("express")
-var mongoose   = require("mongoose")
-var bodyParser = require("body-parser")
+var express    = require("express");
+var mongoose   = require("mongoose");
+var hbs        = require("hbs");
+var bodyParser = require("body-parser");
 
 var app        = express();
+
+app.set("view engine", "hbs");
 app.listen(4000, function(){
   console.log("listening on port 4000");
+})
+
+app.get("/", function(req, res){
+  res.render("index.hbs", {})
 })
